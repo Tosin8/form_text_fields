7123594c3000f5c31858869b5f4939f72bdd2f99
Tosin8 <tosinezekiel8@gmail.com>
2022-07-04 14:52:10 +0100

    Adding Personal Data Class

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,41 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(home: TextFieldDemo()));
+  runApp(const MaterialApp(home: HomePage()));
 }
 
-class TextFieldDemo extends StatelessWidget {
-  const TextFieldDemo({Key? key}) : super(key: key);
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      automaticallyImplyLeading: false,
-      title: const Text('Form Texts'),
-    ));
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: const Text('Form Texts'),
+      ),
+      body: const TextFieldDemo(),
+    );
   }
+}
+
+class TextFieldDemo extends StatefulWidget {
+  const TextFieldDemo({Key? key}) : super(key: key);
+
+  @override
+  State<TextFieldDemo> createState() => _TextFieldDemoState();
+}
+
+class _TextFieldDemoState extends State<TextFieldDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class PersonData {
+  String name = '';
+  String phoneNumber = '';
+  String email = '';
+  String password = '';
 }
