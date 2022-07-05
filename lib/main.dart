56d0669169e7810665fe3e241f8fc316bf75f24d
Tosin8 <tosinezekiel8@gmail.com>
2022-07-05 10:14:00 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(home: HomePage()));
+  runApp(
+    MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(primarySwatch: Colors.green),
+        home: const HomePage()),
+  );
 }
 
 class HomePage extends StatelessWidget {
@@ -26,7 +31,79 @@ class TextFieldDemo extends StatefulWidget {
   State<TextFieldDemo> createState() => _TextFieldDemoState();
 }
 
-class _TextFieldDemoState extends State<TextFieldDemo> {
+class _TextFieldDemoState extends State<TextFieldDemo> with RestorationMixin {
+  PersonData person = PersonData();
+
+  late FocusNode _phoneNumber, _email, _lifeStory, _password, _retypePassword;
+
+  @override
+  void initState() {
+    super.initState();
+    _phoneNumber = FocusNode();
+    _email = FocusNode();
+    _lifeStory = FocusNode();
+    _password = FocusNode();
+    _retypePassword = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    _phoneNumber.dispose();
+    _email.dispose();
+    _lifeStory.dispose();
+    _password.dispose();
+    _retypePassword.dispose();
+    super.dispose();
+  }
+
+  void showInSnackBar(String value) {
+    ScaffoldMessenger.of(context).hideCurrentSnackBar();
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text(value),
+    ));
+  }
+
+  @override
+  String get restorationId => 'text_field_demo';
+
+  @override
+  void restoreState(RestorationBucket oldBucket, bool initialRestore) {
+    registerForRestoration(_autoValidateModeIndex, 'autovalidate_mode');
+  }
+
+  final RestorableInt _autoValidateModeIndex =
+      RestorableInt(AutovalidateMode.disabled.index);
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormKey>(); 
+  final GlobalKey<FormFieldState><String>> final _passwordFieldKey = GlobalKey<FormFieldState<String>>();
+  final _UsNumberTextInputFormatter _phoneNumberFormatter = _UsNumberTextInputFormatter(); 
+
+  void _handleSubmitted() {
+    final form = _formKey.currentState; 
+    if(!form.validate()) {
+      _autoValidateModeIndex.value = AutovalidateMode.always.index;  // Start validating on every change . 
+      showInSnackBar(
+        TextFieldFormErrors()
+        
+      ); 
+    } else {
+      form.save(); 
+      showInSnackBar(
+        TextFieldNameHasPhoneNumber(person.name, person.phoneNumber)); 
+    }
+  }
+
+  String _validateName(String value) {
+    if(value.isEmpty){
+      return TextFieldNameRequired(); 
+    }
+
+    final nameExp = RegExp(r'^[A-Za-z ] +$'); 
+    if (!nameExp.hasMatch(value)){
+      
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
